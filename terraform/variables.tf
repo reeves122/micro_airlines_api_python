b9f46557efef7ee80cbff2a0e52316661209d6f1
reeves122 <reeves122@gmail.com>
2021-10-22 23:11:44 -0500

    Tf workflows (#24)
    * add tflint
    
    * terraform fmt
    
    * unlock tflint version
    
    * add check, plan, apply workflows

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,13 +7,13 @@ variable "name" {
 }
 
 resource "random_string" "bucket_suffix" {
-  length = 8
-  lower = true
-  upper = false
+  length  = 8
+  lower   = true
+  upper   = false
   special = false
 }
 
 locals {
   bucket_name = "micro-airlines-api-${var.env}-${random_string.bucket_suffix.result}"
-  lambda_uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.micro_airlines_api_lambda.arn}/invocations"
+  lambda_uri  = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.micro_airlines_api_lambda.arn}/invocations"
 }
